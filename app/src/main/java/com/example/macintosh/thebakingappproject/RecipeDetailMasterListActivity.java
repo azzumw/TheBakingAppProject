03959f4a16bbb7749d802368ebf336560348eed4
awaqar <azzumwaqar@gmail.com>
2018-11-24 16:50:33 +0000

    Added Parcelable Set Title For Activity based on Recipe clicked

--- app/src/main/java/com/example/macintosh/thebakingappproject/RecipeDetailMasterListActivity.java
+++ app/src/main/java/com/example/macintosh/thebakingappproject/RecipeDetailMasterListActivity.java
@@ -1,13 +1,27 @@
 package com.example.macintosh.thebakingappproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.macintosh.thebakingappproject.Models.Recipe;
+
 public class RecipeDetailMasterListActivity extends AppCompatActivity {
 
+
+    Recipe recipe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail_master_list);
+
+        Intent intent = getIntent();
+
+        recipe = intent.getParcelableExtra("recipe");
+
+        String recipeName = recipe.getName();
+
+        getSupportActionBar().setTitle(recipeName);
     }
+
 }
