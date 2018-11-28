7a24195b637e2bb7e2caeb64bd08fe9987958d62
awaqar <azzumwaqar@gmail.com>
2018-11-28 23:15:08 +0000

    can read bundle data in SteoIngredDetailFragment

--- app/src/main/java/com/example/macintosh/thebakingappproject/StepIngredDetailFragment.java
+++ app/src/main/java/com/example/macintosh/thebakingappproject/StepIngredDetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.macintosh.thebakingappproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.macintosh.thebakingappproject.Models.Steps;
 
 public class StepIngredDetailFragment extends Fragment {
 
@@ -19,9 +23,20 @@ public class StepIngredDetailFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_steps_ingred_details,container,false);
 
-        ImageView imageView = rootview.findViewById(R.id.imageviewFragStepsIngredDetails);
+//        ImageView imageView = rootview.findViewById(R.id.imageviewFragStepsIngredDetails);
+        TextView textView = rootview.findViewById(R.id.textView2);
 
-        imageView.setImageResource(R.drawable.head1);
+        Intent intent = getActivity().getIntent();
+
+        Bundle bundle = intent.getBundleExtra("bundle");
+
+        Steps step = bundle.getParcelable("step");
+
+        String desc = step.getShortDescription();
+
+//        imageView.setImageResource(R.drawable.head1);
+
+        textView.setText(desc);
 
         return rootview;
     }
